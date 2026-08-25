2347c40d066c99a9c0f78a4c499d4d5f1eecf226
Thurzin_Du_.Grau.ff545 <arthuralmeida27borges@gmail.com>
2026-08-24 23:53:46 -0300

    avanço na tela de login
    encaixa no canto

--- projeto/src/pages/login/login.jsx
+++ projeto/src/pages/login/login.jsx
@@ -3,16 +3,28 @@ import "./login.css";
 function Login() {
   return (
     <section className="fundo">
-    <div className="login">
-      <div className="itens">
-        <h1>só para testar</h1>
-        <input type="text" placeholder="Digite seu nome/usuario" required/>
-        <input type="password" placeholder="Senha" required></input>
-        <p>clique aqui</p>
+      <div className="login">
+        <div className="itens">
+          <h1>só para testar</h1>
+          <div className="inpp">
+            <h2>E-mail</h2>
+            <div className="inpeimg">
+              <input type="text" placeholder="Digite seu nome/usuario" required />
+              <img src="/images/email.png" alt="" className="icone-input  " />
+            </div>
+          </div>
+          <div className="inpp2">
+            <h2>Senha</h2>
+            <div className="inpeimg">
+              <input type="password" placeholder="Senha" required></input>
+              <img src="/images/senha.png" alt="" className="icone-input" />
+            </div>
+          </div>
+          <a className="bott" href="#">Entra</a>
         </div>
-    </div>
+      </div>
     </section>
   );
 }
 
-export default Login;
+export default Login; 
