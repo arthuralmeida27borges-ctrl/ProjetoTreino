3b71cd1a18efaded3a86d397648b3d489a6c5e8f
Thurzin_Du_.Grau.ff545 <arthuralmeida27borges@gmail.com>
2026-08-26 22:51:41 -0300

    1/2 finish

---
+++ projeto/src/pages/apps/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../login/index.jsx';
+
+function App() {
+  return (
+    <div className='nn'></div>
+  );
+}
+
+export default App;
---
+++ projeto/src/pages/login/index.jsx
@@ -0,0 +1,42 @@
+import "./index.css";
+
+function Login() {
+  return (
+    <section className="fundo">
+      <div className="login">
+        <div className="itens">
+          <div className="ini">
+            <h1>Entrar na sua conta</h1>
+            <h2>Digite seus dados para continuar</h2>
+          </div>
+          <div className="inpp">
+            <h2>E-mail</h2>
+            <div className="inpeimg">
+              <input type="text" placeholder="Digite seu nome/usuario" required />
+              <img src="./assets/images/email.png" alt="" className="icone-input  " />
+            </div>
+          </div>
+          <div className="inpp2">
+            <h2>Senha</h2>
+            <div className="inpeimg">
+              <input type="password" placeholder="Senha" required></input>
+              <img src="./assets/images/senha.png" alt="" className="icone-input" />
+            </div>
+          </div>
+          <a className="bott" href="#">Entra</a>
+          <div className="divisor">
+            <span className="linha"></span>
+            <span className="texto-divisor">ou</span>
+            <span className="linha"></span>
+          </div>
+          <div className="gogui">
+            <img src="./assets/images/google.webp" alt=""/>
+            <p>Entrar com Google</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Login; 
