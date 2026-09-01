42a6f67c6593198e22517b7986025cc09fcd386b
rafisbbb <m8328224@gmail.com>
2026-08-31 23:40:03 -0300

    67

--- projeto/src/pages/login/index.jsx
+++ projeto/src/pages/login/index.jsx
@@ -2,97 +2,175 @@ import "./index.css";
 import { Link } from "react-router-dom";
 
 function Login() {
-  return (
-    <section className="fundo">
-<<<<<<< HEAD
-      <div>
-      <img src="./assets/images/adiv.png" alt="" className="adiv-png"/>
-       <h1 className="bem-vindo">BEM VINDOS</h1>
-      </div>
-=======
+return ( <section className="fundo">
 
-      <section className="fundo-azul">
-        <div className="maior">
-          <div className="adiv"></div>
-          <img className='adiv-img' src='/assets/images/adiv.png' alt='' />
-          <div className="texts">
-            <h1>Bem vindos de <span className="cor">Volta</span></h1>
-            <h2>Faça login para acessar sua conta
-              <br />e continuar de onde parou</h2>
-          </div>
-        <div className="iconis">
-          <div className="Escudo">
-            <img src="./assets/images/escudo.png" alt="" />
-            <h1>Seguro e confiavel</h1>
-            <h2>Seus dados protegidos com criptografia de ponta a ponta</h2>
-          </div>
-          <div className="estastic">
-            <img style={{ width: "80px" }} src="./assets/images/Estástitica.png" alt="" />
-            <h1>Acesse suas informações</h1>
-            <h2>Visualize seu historico de compras, 
-              <br/>atividades e muitas coisas</h2>
-          </div>
-          <div className="raio">
-            <img src="./assets/images/raio.png" alt="" />
-            <h1>Rápido e fácil</h1>
-            <h2>Uma experiecia simples e intuitiva 
-              <br/>para o seu dia a dia</h2>
-          </div>
+```
+  <div>
+    <img
+      src="./assets/images/adiv.png"
+      alt=""
+      className="adiv-png"
+    />
+
+    <h1 className="bem-vindo">BEM VINDOS</h1>
+  </div>
+
+  <section className="fundo-azul">
+    <div className="maior">
+
+      <div className="adiv"></div>
+
+      <img
+        className="adiv-img"
+        src="/assets/images/adiv.png"
+        alt=""
+      />
+
+      <div className="texts">
+        <h1>
+          Bem vindos de <span className="cor">Volta</span>
+        </h1>
+
+        <h2>
+          Faça login para acessar sua conta
+          <br />
+          e continuar de onde parou
+        </h2>
+      </div>
+
+      <div className="iconis">
+
+        <div className="Escudo">
+          <img src="./assets/images/escudo.png" alt="" />
+
+          <h1>Seguro e confiável</h1>
+
+          <h2>
+            Seus dados protegidos com criptografia de ponta a ponta
+          </h2>
         </div>
+
+        <div className="estastic">
+          <img
+            style={{ width: "80px" }}
+            src="./assets/images/Estástitica.png"
+            alt=""
+          />
+
+          <h1>Acesse suas informações</h1>
+
+          <h2>
+            Visualize seu histórico de compras,
+            <br />
+            atividades e muitas coisas
+          </h2>
         </div>
-      </section>
-<<<<<<< HEAD
->>>>>>> 48b7fbd342974217571261083e85a599692cf14b
-=======
-      {/* A tela de login inteira é daqui pra baixo */}
->>>>>>> d7f30e8a4ebd00c9d6eb850716835b3edf9c3769
-      <div className="login">
-        <div className="itens">
-          <div className="ini">
-            <h1>Entrar na sua conta</h1>
-            <h2>Digite seus dados para continuar</h2>
+
+        <div className="raio">
+          <img src="./assets/images/raio.png" alt="" />
+
+          <h1>Rápido e fácil</h1>
+
+          <h2>
+            Uma experiência simples e intuitiva
+            <br />
+            para o seu dia a dia
+          </h2>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  {/* Tela de login */}
+  <div className="login">
+    <div className="itens">
+
+      <div className="ini">
+        <h1>Entrar na sua conta</h1>
+        <h2>Digite seus dados para continuar</h2>
+      </div>
+
+      <div className="inpp">
+        <h2>E-mail</h2>
+
+        <div className="inpeimg">
+          <input
+            type="text"
+            placeholder="Digite seu nome/usuário"
+            required
+          />
+
+          <img
+            src="./assets/images/email.png"
+            alt=""
+            className="icone-input"
+          />
+        </div>
+      </div>
+
+      <div className="inpp2">
+        <h2>Senha</h2>
+
+        <div className="inpeimg">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+          />
+
+          <img
+            src="./assets/images/senha.png"
+            alt=""
+            className="icone-input"
+          />
+        </div>
+
+        <div className="ultimo">
+
+          <div className="check">
+            <input type="checkbox" />
+            <p>Lembrar-me</p>
           </div>
-          <div className="inpp">
-            <h2>E-mail</h2>
-            <div className="inpeimg">
-              <input type="text" placeholder="Digite seu nome/usuario" required />
-              <img src="./assets/images/email.png" alt="" className="icone-input  " />
-            </div>
+
+          <div className="esqueci">
+            <Link className="esqueciLI" to="/semSenha">
+              <p>Esqueceu a senha?</p>
+            </Link>
           </div>
-          <div className="inpp2">
-            <h2>Senha</h2>
-            <div className="inpeimg">
-              <input type="password" placeholder="Senha" required></input>
-              <img src="./assets/images/senha.png" alt="" className="icone-input" />
-            </div>
-            <div className="ultimo">
-              <div className="check">
-                <input type="checkbox" />
-                <p>Lembrar-me</p>
-              </div>
-              <div className="esqueci">
-                <Link className="esqueciLI" to='/semSenha'><p>Esqueceu a senha?</p></Link>
-              </div>
-            </div>
-          </div>
-          <a className="bott" href="#">Entra</a>
-          <div className="divisor">
-            <span className="linha"></span>
-            <span className="texto-divisor">ou</span>
-            <span className="linha"></span>
-          </div>
-          <div className="gogui">
-            <img src="./assets/images/google.webp" alt="" />
-            <p>Entrar com Google</p>
-          </div>
-          <div className="naotem">
+
+        </div>
+      </div>
+
+      <a className="bott" href="#">
+        Entrar
+      </a>
+
+      <div className="divisor">
+        <span className="linha"></span>
+        <span className="texto-divisor">ou</span>
+        <span className="linha"></span>
+      </div>
+
+      <div className="gogui">
+        <img src="./assets/images/google.webp" alt="" />
+        <p>Entrar com Google</p>
+      </div>
+
+      <div className="naotem">
         <h2>Não tem conta?</h2>
-        <Link className="esqueciLI" to='/criação'>Clique aqui para se casdastrar</Link>
+
+        <Link className="esqueciLI" to="/criação">
+          Clique aqui para se cadastrar
+        </Link>
       </div>
-        </div>
-      </div>
-    </section>
-  );
+
+    </div>
+  </div>
+
+</section>
+
+);
 }
 
-export default Login; 
+export default Login;
